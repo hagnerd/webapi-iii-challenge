4667db2a232820f19de7d502489103412a9f9c93
Matt Hagner <web@matthagner.com>
2019-08-13 08:21:26 -0500

    Implement logger middleware

--- server.js
+++ server.js
@@ -2,14 +2,18 @@ const express = 'express';
 
 const server = express();
 
-server.get('/', (req, res) => {
+server.use(logger);
+
+server.get('/', (_req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
 
 //custom middleware
 
-function logger(req, res, next) {
+function logger(req, _res, next) {
+  console.log(`${req.method} - ${req.url} - ${new Date()}`);
 
+  next();
 };
 
 module.exports = server;
