7e0f15fff6e03a769d5f39be062ffa0e58f85550
Matt Hagner <web@matthagner.com>
2019-08-13 10:00:02 -0500

    Implement DELETE handler for api/users/:id

--- users/userRouter.js
+++ users/userRouter.js
@@ -70,7 +70,20 @@ router.get("/:id/posts", validateUserId, async (req, res) => {
   }
 });
 
-router.delete("/:id", (req, res) => {});
+router.delete("/:id", validateUserId, async (req, res) => {
+  try {
+    await User.remove(req.user.id);
+
+    res.status(200).json({
+      message: "Successfully deleted user",
+      user: req.user
+    });
+  } catch (error) {
+    res.status(500).json({
+      errorMessage: "Internal server error"
+    });
+  }
+});
 
 router.put("/:id", (req, res) => {});
 
