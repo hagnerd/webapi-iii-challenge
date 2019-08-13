803abac9ca3aede55ca43d41e6f0b9e0342e93be
Matt Hagner <web@matthagner.com>
2019-08-13 08:31:33 -0500

    Set up server

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// code away!
+const server = require("./server");
+
+const PORT = process.env.PORT || 4000;
+
+server.listen(PORT, () => {
+  console.log(`Server now listening on Port: ${PORT}`);
+});
