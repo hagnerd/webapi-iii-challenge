ac14372fd23f098f3162948b061a03d996be19e1
Matt Hagner <web@matthagner.com>
2019-08-13 08:31:20 -0500

    Fix error with require

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
-const express = 'express';
+const express = require("express");
+const userRouter = require("./users/userRouter");
 
 const server = express();
 
 server.use(logger);
 
-server.get('/', (_req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+server.use("/api/users", userRouter);
+
+server.get("/", (_req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
@@ -14,6 +17,6 @@ function logger(req, _res, next) {
   console.log(`${req.method} - ${req.url} - ${new Date()}`);
 
   next();
-};
+}
 
 module.exports = server;
