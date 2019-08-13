cc74f7b34a9af67666af78239cfe537783f40ee4
Matt Hagner <web@matthagner.com>
2019-08-13 08:32:06 -0500

    Implement GET for /api/users
    - Route fetches all users from the database

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,46 @@
-const express = 'express';
+const express = require("express");
+const User = require("./userDb");
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {});
 
+router.post("/:id/posts", (req, res) => {});
+
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.get();
+
+    res.status(200).json({
+      users
+    });
+  } catch (error) {
+    res.status(500).json({
+      errorMessage: "Internal server error"
+    });
+  }
 });
 
-router.post('/:id/posts', (req, res) => {
+router.get("/:id", (req, res) => {});
 
-});
+router.get("/:id/posts", (req, res) => {});
 
-router.get('/', (req, res) => {
+router.delete("/:id", (req, res) => {});
 
-});
-
-router.get('/:id', (req, res) => {
-
-});
-
-router.get('/:id/posts', (req, res) => {
-
-});
-
-router.delete('/:id', (req, res) => {
-
-});
-
-router.put('/:id', (req, res) => {
-
-});
+router.put("/:id", (req, res) => {});
 
 //custom middleware
 
-function validateUserId(req, res, next) {
+async function validateUserId(req, res, next) {
+  const { id } = req.params;
 
-};
+  try {
+    const user = User.getById(id);
+  } catch (error) {}
+}
 
-function validateUser(req, res, next) {
+function validateUser(req, res, next) {}
 
-};
-
-function validatePost(req, res, next) {
-
-};
+function validatePost(req, res, next) {}
 
 module.exports = router;
